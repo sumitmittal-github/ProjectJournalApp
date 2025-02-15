546e88703d5023bf6259e9322c706bcb7e4bd62a
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-02-15 13:01:24 -0500

    Implemented Log4j2 framework for custom logging

--- src/main/java/com/sumit/controller/AdminController.java
+++ src/main/java/com/sumit/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.sumit.controller;
 
 import com.sumit.entity.User;
 import com.sumit.service.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
+@Log4j2
 public class AdminController {
 
     @Autowired
@@ -18,7 +20,9 @@ public class AdminController {
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> findAllUsers() {
+        log.info("Entry AdminController.findAllUsers ...");
         List<User> list = userService.findAll();
+        log.info("Exit AdminController.findAllUsers !!!");
         if(list != null && !list.isEmpty())
             return new ResponseEntity<>(list, HttpStatus.OK);
         else
--- src/main/java/com/sumit/controller/PublicController.java
+++ src/main/java/com/sumit/controller/PublicController.java
@@ -2,6 +2,7 @@ package com.sumit.controller;
 
 import com.sumit.entity.User;
 import com.sumit.service.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public")
+@Log4j2
 public class PublicController {
 
     @Autowired
@@ -16,6 +18,12 @@ public class PublicController {
 
     @GetMapping("/health-check")
     public ResponseEntity<String> healthCheck() {
+        log.trace("Trace");
+        log.debug("Debug");
+        log.info("Info");
+        log.warn("Warn");
+        log.error("Error");
+        log.fatal("Fatal");
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
